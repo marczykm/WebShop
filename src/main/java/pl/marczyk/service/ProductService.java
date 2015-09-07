f8f5f4a1310870220968a3a9251243ee5c569a47
Marcin Marczyk <m.l.marczyk@gmail.com>
2015-09-07 21:42:14 +0200

    added generated testdata

--- src/main/java/pl/marczyk/service/ProductService.java
+++ src/main/java/pl/marczyk/service/ProductService.java
@@ -3,6 +3,7 @@ package pl.marczyk.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.marczyk.domain.ProductRepository;
+import pl.marczyk.model.Product;
 
 /**
  * Created by marcin on 07.09.15.
@@ -12,4 +13,8 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public Iterable<Product> findAll(){
+        return productRepository.findAll();
+    }
 }
