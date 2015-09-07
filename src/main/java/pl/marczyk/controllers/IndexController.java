750144fc6837669c88a464bd9ba8dc761aae8376
Marcin Marczyk <m.l.marczyk@gmail.com>
2015-09-07 23:12:13 +0200

    added products viewing

--- src/main/java/pl/marczyk/controllers/IndexController.java
+++ src/main/java/pl/marczyk/controllers/IndexController.java
@@ -2,6 +2,7 @@ package pl.marczyk.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.marczyk.service.ProductService;
 
@@ -15,7 +16,8 @@ public class IndexController {
     private ProductService productService;
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("products", productService.findAll());
         return "index";
     }
 }
